c719e58f0d27670e271dd056f13efa869aa358b8
cryan-code1 <cryan.code@gmail.com>
2025-05-09 20:20:26 -0300

    feat: criação da classe para exercicio 4

---
+++ TargetSistemasTest/ConsoleApp2/Tests/Test4_FaturamentoPorEstado.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp2.Tests;
+
+public static class Test4_FaturamentoPorEstado
+{
+    public static void Executar()
+    {
+        var faturamentos = new Dictionary<string, decimal>
+        {
+            { "SP", 67836.43m },
+            { "RJ", 36678.66m },
+            { "MG", 29229.88m },
+            { "ES", 27165.48m },
+            { "Outros", 19849.53m }
+        };
+
+        decimal total = faturamentos.Values.Sum();
+
+        Console.WriteLine("[Exercicio 4] Percentual por estado:");
+        foreach (var estado in faturamentos)
+        {
+            decimal percentual = estado.Value / total * 100;
+            Console.WriteLine($"{estado.Key}: {percentual:F2}%");
+        }
+        Console.WriteLine();
+    }
+}
