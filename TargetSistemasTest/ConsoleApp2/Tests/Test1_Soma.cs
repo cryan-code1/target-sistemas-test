b82174915d6d28ced255d1f4ddbd8e4e2ebea92b
cryan-code1 <cryan.code@gmail.com>
2025-05-09 20:20:11 -0300

    feat: criação da classe para exercicio 1

---
+++ TargetSistemasTest/ConsoleApp2/Tests/Test1_Soma.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp2.Tests;
+
+public static class Test1_Soma
+{
+    public static void Executar()
+    {
+        int soma = 0;
+        for (int k = 1; k <= 13; k++)
+        {
+            soma += k;
+        }
+
+        Console.WriteLine($"[Exercicio 1] Soma final: {soma}\n");
+    }
+}
