4be01fb72ed26ba0a3989cf7a0ee9d8444b281a0
cryan-code1 <cryan.code@gmail.com>
2025-05-09 20:20:16 -0300

    feat: criação da classe para exercicio 2

---
+++ TargetSistemasTest/ConsoleApp2/Tests/Test2_Fibonacci.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp2.Tests;
+
+public static class Test2_Ficonacci
+{
+    public static void Executar()
+    {
+        int numeroAlvo = 21;
+        int anterior = 0, atual = 1;
+
+        while (atual < numeroAlvo)
+        {
+            int temp = atual;
+            atual = anterior + atual;
+            anterior = temp;
+        }
+
+        string resultado = (atual == numeroAlvo || numeroAlvo == 0)
+            ? "pertence à sequência de Fibonacci."
+            : "não pertence à sequência de Fibonacci.";
+
+        Console.WriteLine($"[Exercicio 2] O número {numeroAlvo} {resultado}\n");
+    }
+}
