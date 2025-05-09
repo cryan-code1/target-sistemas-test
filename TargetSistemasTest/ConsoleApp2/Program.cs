21d1889001f0cab7ebfb333907c8383c5fb03a26
cryan-code1 <cryan.code@gmail.com>
2025-05-09 20:20:48 -0300

    feat: criação da classe Program com a chamada de todos os exercicios

---
+++ TargetSistemasTest/ConsoleApp2/Program.cs
@@ -0,0 +1,7 @@
+﻿using ConsoleApp2.Tests;
+
+Test1_Soma.Executar();
+Test2_Ficonacci.Executar(); 
+Test3_FaturamentoDiario.Executar();
+Test4_FaturamentoPorEstado.Executar();
+Test5_InverterString.Executar();
