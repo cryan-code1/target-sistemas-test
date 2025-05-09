279012a3569e0217bf363ebc103e791319194e19
cryan-code1 <cryan.code@gmail.com>
2025-05-09 20:20:33 -0300

    feat: criação da classe para exercicio 5

---
+++ TargetSistemasTest/ConsoleApp2/Tests/Test5_InverterString.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp2.Tests;
+
+public class Test5_InverterString
+{
+    public static void Executar()
+    {
+        string texto = "Exemplo de string";
+        char[] caracteres = texto.ToCharArray();
+        string invertido = "";
+
+        for (int i = caracteres.Length - 1; i >= 0; i--)
+        {
+            invertido += caracteres[i];
+        }
+
+        Console.WriteLine($"[Exercicio 5] String invertida: {invertido}");
+    }
+}
